35d6d5f930d6b6d02e2ac8e098b2ce16e989828f
Luan Leone <luanleone05@gmail.com>
2021-03-18 00:46:39 -0300

    :rocket: Componentizing the application.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+import { Header } from "./Header"
+import '../styles/content.scss';
+
+interface ContentProps{
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+  selectedGenre: string;
+};
+
+export function Content( { movies, selectedGenre } : ContentProps) {
+  return(
+  <div className="container">
+    <Header title={selectedGenre}/>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
